2109d1fd6e59f2151273d41f3970eddd21c12fcd
MUSKAAN SINHA <88770752+ENG18CS0178-MUSKAANSINHA@users.noreply.github.com>
2025-08-28 16:58:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import json
+
+app = Flask(__name__)
+app.secret_key = "mysecret"   # for session login
+
+# Load users
+def load_users():
+    try:
+        with open("users.json", "r") as f:
+            return json.load(f)
+    except:
+        return {}
+
+# Save users
+def save_users(users):
+    with open("users.json", "w") as f:
+        json.dump(users, f, indent=4)
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        users = load_users()
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if email in users and users[email]["password"] == password:
+            session["user"] = email
+            return redirect(url_for("dashboard"))
+        else:
+            return "Invalid credentials"
+    return render_template("login.html")
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        users = load_users()
+        email = request.form["email"]
+        password = request.form["password"]
+
+        if email not in users:
+            users[email] = {"password": password}
+            save_users(users)
+            return redirect(url_for("login"))
+        else:
+            return "User already exists!"
+    return render_template("signup.html")
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    if "user" not in session:
+        return redirect(url_for("login"))
+
+    if request.method == "POST":
+        goal = request.form["goal"]
+        with open("exercises.json", "r") as f:
+            exercises = json.load(f)
+
+        return render_template("workout.html", workouts=exercises[goal])
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
